4bf3c250ac84db9e93579dfeb00ceafcaf9cd4f7
andrecouto <andrecouto.jc.95@gmail.com>
2022-03-02 19:58:36 -0300

    Classe Gato criada.

--- java-projects/MeuPrimeiroPrograma-eclipse/src/com/dio/MeuPrimeiroPrograma.java
+++ java-projects/MeuPrimeiroPrograma-eclipse/src/com/dio/MeuPrimeiroPrograma.java
@@ -1,12 +1,26 @@
 package com.dio;
 
+import com.dio.model.Gato;
+
 public class MeuPrimeiroPrograma {
 
 	public static void main(String[] args) {
+		/*
 		int a = 2, b = 3;
 
 		System.out.println("Hello World!!!" + (a+b));
-
+		*/
+		
+		Gato gato = new Gato();
+		Livro livro = new Livro(); 
+		
+		System.out.println(gato);
+		System.out.println(livro); // printa o endereço de memoria -> faltou o toString
 	}
 
+}
+
+class Livro {
+	private String nome;
+	private Integer numpag;
 }
