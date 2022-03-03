045ac2b23fa00a0feb551306ff279c5421e35633
andrecouto <andrecouto.jc.95@gmail.com>
2022-03-03 17:10:29 -0300

    Classe Livro criada.

--- java-projects/primeiro-projeto-intelliJ/src/br/com/dio/PrimeiroPrograma.java
+++ java-projects/primeiro-projeto-intelliJ/src/br/com/dio/PrimeiroPrograma.java
@@ -8,7 +8,46 @@ public class PrimeiroPrograma {
         System.out.println("Hello World!" + (a + b));*/
 
         Gato gato = new Gato();
+        Livro livro1 = new Livro();
         System.out.println(gato);
+        System.out.println();
+        System.out.println(livro1);
 
+    }
+}
+class Livro {
+    private String nome;
+    private Integer numPag;
+
+    public Livro() {
+    }
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
     }
 }
